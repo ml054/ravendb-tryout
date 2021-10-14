eb95c96e463fced057485a9608100c648abb5a85
Tomasz Opalach <tomtom@ravendb.net>
2021-10-14 18:13:37 +0200

    add boilerplate code

---
+++ RavenDB.Tryout.App/DocumentStoreHolder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Raven.Client.Documents;
+
+namespace RavenDB.Tryout.App
+{
+    public class DocumentStoreHolder
+    {
+        private static readonly Lazy<IDocumentStore> LazyStore =
+            new Lazy<IDocumentStore>(() =>
+            {
+                var store = new DocumentStore
+                {
+                    Urls = new[] { "http://localhost:8080" },
+                    Database = "CRUD"
+                };
+
+                return store.Initialize();
+            });
+
+        public static IDocumentStore Store => LazyStore.Value;
+    }
+}
---
+++ RavenDB.Tryout.App/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace RavenDB.Tryout.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No argument provided.");
+                return;
+            }
+            
+            var arg = args[0];
+
+            switch (arg)
+            {
+                case "create":
+                    CreateDocument();
+                    break;
+                
+                case "read":
+                case "update":
+                    ReadAndUpdateDocument();
+                    break;
+                
+                case "delete":
+                    DeleteDocument();
+                    break;
+                
+                default:
+                    Console.WriteLine("No matching argument provided.");
+                    break;
+            }
+        }
+
+        private static void CreateDocument()
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        private static void ReadAndUpdateDocument()
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        private static void DeleteDocument()
+        {
+            throw new NotImplementedException("TODO");
+        }
+    }
+}
